ebb01c3db617f39a70f5cd86c05bbb2868c93ae2
kanchan2510 <baskarane.kanchandiran@gmail.com>
2024-10-09 22:55:40 +0200

    Interface utilisateur

--- Puissance_4/JS/Javascript.js
+++ Puissance_4/JS/Javascript.js
@@ -3,3 +3,7 @@ const lignes = 6;
 let joueurActuel = 'rouge';
 let plateauJeu = [];
 let partieTerminee = false;
+
+const elementPlateau = document.getElementById('game-board');
+const elementMessage = document.getElementById('message');
+
