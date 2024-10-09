302748af0a877cbb52f574ed6e38ea3cdb0ad184
kanchan2510 <baskarane.kanchandiran@gmail.com>
2024-10-09 23:10:47 +0200

    Creation de plteau de jeu

--- Puissance_4/JS/Javascript.js
+++ Puissance_4/JS/Javascript.js
@@ -7,3 +7,20 @@ let partieTerminee = false;
 const elementPlateau = document.getElementById('game-board');
 const elementMessage = document.getElementById('message');
 
+// Initialiser le plateau de jeu
+
+function creerPlateau() {
+    plateauJeu = [];
+    for (let r = 0; r < lignes; r++) {
+        const ligne = [];
+        for (let c = 0; c < colonnes; c++) {
+            const cellule = document.createElement('div');
+            cellule.classList.add('cell', 'empty');
+            cellule.dataset.colonne = c;
+            elementPlateau.appendChild(cellule);
+            ligne.push(null);
+        }
+        plateauJeu.push(ligne);
+    }
+}
+
